438a54bdcfebc5785ac77ad24667c53f6f7b0397
Ashton Smith <AshtonSmith@ashtons-mbp.mynetworksettings.com>
2025-02-16 11:23:08 -0500

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    root: true,
+    env: {
+      node: true
+    },
+    extends: [
+      'plugin:vue/essential',
+      'eslint:recommended'
+    ],
+    parserOptions: {
+      parser: '@babel/eslint-parser'
+    },
+    rules: {
+      // Add any specific rules here if you want to override defaults
+    }
+  }
---
+++ my-vue-app/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <h1>Hello Vue!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<style>
+#app {
+  text-align: center;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+}
+</style>
+
+
